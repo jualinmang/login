a76895cd400d338806d4516eb1ea3e3b294e37f1
Rolly Maulana Awangga <awangga@gmail.com>
2024-08-11 01:28:27 +0700

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import { wauthparam } from "https://cdn.jsdelivr.net/gh/whatsauth/js@0.2.1/confi
 
 wauthparam.auth_ws="d3NzOi8vYXBpLndhLm15LmlkL3dzL3doYXRzYXV0aC9wdWJsaWM=";
 //wauthparam.keyword="aHR0cHM6Ly93YS5tZS82Mjg5NTgwMDAwNjAwMD90ZXh0PXdoNHQ1YXV0aDA=";
-wauthparam.keyword="aHR0cHM6Ly93YS5tZS9wYW1vbmdkZXNhP3RleHQ9d2g0dDVhdXRoMA==";
+wauthparam.keyword="aHR0cHM6Ly93YS5tZS82MjgxNTEwMDQwMDIwP3RleHQ9d2g0dDVhdXRoMA==";
 wauthparam.tokencookiehourslifetime=18;
 wauthparam.redirect ="/auth"
 deleteCookie(wauthparam.tokencookiename);
